6e949c185491dc5c575713a8583083c0628b2878
Emmanuel <emmanuelcruz5@gmail.com>
2022-08-22 14:32:40 -0400

    refactored for commands to be more dynamic

--- bash.js
+++ bash.js
@@ -1,13 +1,19 @@
 //Output a prompt
 process.stdout.write("prompt > ");
 
+const validCommands = ["pwd"];
+const cwd = process.cwd(); //the current working directory
+
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on("data", (data) => {
-  const cwd = process.cwd(); //the current working directory
+  if (!validCommands.includes(data.toString().trim())) {
+    process.stdout.write(
+      `"${data.toString().trim()}"` + " is not a vaild command"
+    );
+  }
+
   if (data.toString().trim() === "pwd") {
     process.stdout.write(cwd);
-  } else {
-    process.stdout.write(data.toString().trim() + "is not a vaild command");
   }
 
   process.stdout.write("\nprompt > ");
